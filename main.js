c727fc66c3328cd52064aec567e441227228df11
gustavomorais <gustavomorais.dev@gmail.com>
2022-12-20 12:57:48 -0300

    adiciona o quadro de pixels 5x5

--- main.js
+++ main.js
@@ -14,6 +14,7 @@ if (localStorage.getItem('colorPalette')) {
 // Captura elementos
 const palette = document.getElementById('color-palette');
 const randomColorsBtn = document.getElementById('button-random-color');
+const board = document.getElementById('pixel-board');
 
 // Coloca as cores na paleta
 const fillPalette = () => {
@@ -32,15 +33,33 @@ const randomColors = () => {
         const randomColor = Math.floor(Math.random() * 16777215).toString(16);
         // -------------------------------------
         const colors = document.getElementsByClassName('color');
-        paletteColors[`${index+1}`] = "#" + randomColor;
+        paletteColors[`${index + 1}`] = "#" + randomColor;
         colors[index].style.backgroundColor = "#" + randomColor;
     }
     localStorage.setItem('colorPalette', JSON.stringify(paletteColors));
 }
 
+// Cria o board
+const createBoard = () => {
+    let counter = 0;
+    for (let index = 0; index < 25; index += 1) {
+        const pixel = document.createElement('div');
+        const br = document.createElement('br');
+        pixel.classList.add('pixel');
+        board.appendChild(pixel);
+        counter += 1
+        if (counter == 5) {
+            board.appendChild(br);
+            counter = 0;
+        }
+    }
+}
+
+
 // Chama funções
 
 fillPalette();
+createBoard();
 
 // Listeners
 
